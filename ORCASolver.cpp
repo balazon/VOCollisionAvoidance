70e0c2999ca692362325f2fa5b396cee9e0f4988
balazon <balazon@gmail.com>
2015-07-22 18:37:06 +0200

    Math utility functions for later use

--- ORCASolver.cpp
+++ ORCASolver.cpp
@@ -1,5 +1,7 @@
 #include "ORCASolver.h"
 
+#include <cmath>
+
 Agent::Agent()
 {
 	std::fill_n(nearbyAgents, CA_MAXNEARBY, -1);
@@ -55,6 +57,61 @@ void ORCASolver::SetParameters(float T)
 {
 	this->T = T;
 }
+
+
+//Math stuff here
+
+bool QuadraticEquation(float a, float b, float c, float& x1, float& x2)
+{
+	float D;
+	if(fabs(a) < EPS)
+	{
+		x1 = x2 = - c / b;
+		return true;
+	}
+	if((D = b * b - 4 * a * c) < 0)
+	{
+		return false;
+	}
+	float denom_reciproc = .5f / a;
+	float sqrtD = sqrtf(D);
+	x1 = (- b + sqrtD) * denom_reciproc;
+	x2 = (- b - sqrtD) * denom_reciproc;
+	return true;
+}
+
+bool IntersectLineCircle(float A, float B, float C, float u, float v, float r, float& x1, float& y1, float& x2, float& y2)
+{
+	bool swapped = false;
+	if(fabs(B) < EPS)
+	{
+		swapped = true;
+		std::swap(u, v);
+		std::swap(A, B);
+	}
+	float a = A * A + B * B;
+	float b = - 2.f * C * A - 2.f * u * B * B + 2.f * v * A * B;
+	float c = C * C - 2.f * v * C * B - r * r * B * B;
+	bool solvable = QuadraticEquation(a, b, c, x1, x2);
+	if(!solvable)
+	{
+		return false;
+	}
+	y1 = (C - A * x1) / B;
+	y2 = (C - A * x2) / B;
+	if(swapped)
+	{
+		std::swap(x1, y1);
+		std::swap(x2, y2);
+	}
+	return true;
+}
+
+bool IntersectCircleCircle(float u1, float v1, float r1, float u2, float v2, float r2, float& x1, float& y1, float& x2, float& y2)
+{
+	return IntersectLineCircle(u1 - u2, v1 - v2, (r2 + r1) * (r2 - r1), u1, v1, r1, x1, y1, x2, y2);
+}
+
 
 
 void ORCASolver::ComputeNewVelocities()
